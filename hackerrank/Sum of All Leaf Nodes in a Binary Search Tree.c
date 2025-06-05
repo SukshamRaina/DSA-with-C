13cf0bd8818054ad85abbfb7d705e24c9825e1bd
Suksham Raina <9796rajni@gmail.com>
2025-06-05 19:50:27 +0530

    Create Sum of All Leaf Nodes in a Binary Search Tree.c

---
+++ hackerrank/Sum of All Leaf Nodes in a Binary Search Tree.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+typedef struct node{
+        struct node* left;
+        int data;
+        struct node* right;
+} node;
+node* create(int value){
+        node * newnode=(node*)malloc(sizeof(node));
+        newnode->data=value;
+        newnode->left=newnode->right=NULL;
+        return newnode;
+}
+node* insert(node* root,int value){
+        if (root==NULL) 
+                return create(value);
+        else if(value<root->data)
+                root->left=insert(root->left,value);
+        else
+                root->right=insert(root->right,value);
+        return root;
+}
+int sumofnodes(node* root){
+    if (root==NULL) return 0;
+        if(root->left==NULL && root ->right==NULL)
+                return root->data;
+        else
+        return sumofnodes(root->left)+sumofnodes(root->right);
+}
+int main() {
+        node* root=NULL;
+        int n,value;
+        scanf("%d",&n);
+        for(int i=0;i<n;i++){
+                scanf("%d",&value);
+                root=insert(root,value);
+
+}
+int sum = sumofnodes(root);
+        printf("%d",sum);
+        return 0;
+    
+}
